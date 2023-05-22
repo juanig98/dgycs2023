500fb92c34e6b898f65bcccf25e8cc0ad139c7a8
juanig98 <juanigalarza98@gmail.com>
2023-05-22 12:49:13 -0300

    animations

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 window.addEventListener('load', () => {
     animateDisplay(document.querySelector('section.main>article'), 'show', 'block', 600)
+
+    console.log(window.innerHeight);
+
 })
 
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
@@ -47,3 +49,37 @@ animateDisplay = function (target, animationClass, displayType, timeout) {
         }, timeout);
     }
 };
+
+
+
+const logo = document.getElementById('logo');
+var initialX = 0;
+var initialY = 0;
+var amplitude = 100; // Ajusta la amplitud de la parábola
+var frequency = 0.02; // Ajusta la frecuencia de la parábola
+const logoWidth = 230;
+
+window.addEventListener('scroll', function () {
+    const yMax = document.body.offsetHeight - window.innerHeight;
+    const xMax = window.innerWidth;
+
+    const scrollPosition = document.documentElement.scrollTop;
+    const propY = scrollPosition / yMax;
+    const x = (propY - .5) * 2;
+    // Posición
+    logo.style.top = (scrollPosition * 0.2) + "px";
+    const desp = xMax * (x + 1) * (x + .6) * (x + .2) * (x - .2) * (x - .6) * (x - 1);
+    console.log({ desp });
+    logo.style.left = 683 + desp + "px";
+    // calculatePositionLogo(yMax, scrollPosition, propY)
+
+    // // Tamaño
+    logo.style.transform = (x < 0)
+        ? 'scale(' + (1 - .35 * x - .35) + ')'
+        : 'scale(' + (0.3 + .35 * x + .35) + ')';
+
+
+    // Rotación
+    logo.style.rotate = (scrollPosition / yMax * 180) + "deg"; // Con 1200 u.scroll gira 180
+});
+
