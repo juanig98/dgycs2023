d5ed5389a50d5e8b7d6f11e56a76bdb67ddbc9bc
Juan <juanigalarza98@gmail.com>
2023-05-30 19:20:31 -0300

    logos and last screen

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 window.addEventListener('load', () => {
     animateDisplay(document.querySelector('section.main>article'), 'show', 'block', 600)
-
-    console.log(window.innerHeight);
-
 })
 
 
@@ -53,25 +50,45 @@ animateDisplay = function (target, animationClass, displayType, timeout) {
 
 
 const logo = document.getElementById('logo');
+const logoS1 = document.getElementById('logo-s-1');
+const logoS2 = document.getElementById('logo-s-2');
+const logoS3 = document.getElementById('logo-s-3');
+const logoS4 = document.getElementById('logo-s-4');
 var initialX = 0;
 var initialY = 0;
 var amplitude = 100; // Ajusta la amplitud de la parábola
 var frequency = 0.02; // Ajusta la frecuencia de la parábola
 const logoHeight = 200;
 const logoWidth = 230;
+const logoWidth1y2 = 150;
+const logoWidth3y4 = 110;
 
 window.addEventListener('scroll', function () {
     const yMax = document.body.offsetHeight - window.innerHeight;
     const xMax = document.body.offsetWidth;
- 
+
     const scrollPosition = document.documentElement.scrollTop;
+    console.log(scrollPosition); 770
     const propY = scrollPosition / yMax;
     const x = (propY - .5) * 2;
     // Posición
-    logo.style.top = (scrollPosition * 0.2) + "px";
     const desp = (yMax - logoHeight) * (x + 1) * (x + .5) * (x) * (x - .5) * (x - 1);
+    const desp1 = (yMax - logoHeight) * (x + 1) * (x + .51) * (x) * (x - .5) * (x - 1);
+    const desp2 = (yMax - logoHeight) * (x + 1) * (x + .49) * (x) * (x - .5) * (x - 1);
+    const desp3 = (yMax - logoHeight) * (x + 1) * (x + .52) * (x) * (x - .5) * (x - 1);
+    const desp4 = (yMax - logoHeight) * (x + 1) * (x + .58) * (x) * (x - .5) * (x - 1);
+
+    logo.style.top = (scrollPosition * 0.2) + "px";
+    logoS1.style.top = (scrollPosition * 0.2) + "px";
+    logoS2.style.top = (scrollPosition * 0.2) + "px";
+    logoS3.style.top = (scrollPosition * 0.2) + "px";
+    logoS4.style.top = (scrollPosition * 0.2) + "px";
+
     logo.style.left = (xMax / 2 - logoWidth / 2) + desp + "px";
-    // calculatePositionLogo(yMax, scrollPosition, propY)
+    logoS1.style.left = (xMax / 2 - logoWidth1y2 / 2) + desp1 + "px";
+    logoS2.style.left = (xMax / 2 - logoWidth1y2 / 2) + desp2 + "px";
+    logoS3.style.left = (xMax / 2 - logoWidth3y4 / 2) + desp3 + "px";
+    logoS4.style.left = (xMax / 2 - logoWidth3y4 / 2) + desp4 + "px";
 
     // // Tamaño
     logo.style.transform = (x < 0)
@@ -81,5 +98,17 @@ window.addEventListener('scroll', function () {
 
     // Rotación
     logo.style.rotate = (scrollPosition / yMax * 180) + "deg"; // Con 1200 u.scroll gira 180
+    logoS1.style.rotate = (scrollPosition / yMax * 180) + "deg";
+    logoS2.style.rotate = (scrollPosition / yMax * 180) + "deg";
+    logoS3.style.rotate = (scrollPosition / yMax * 180) + 60 + "deg";
+    logoS4.style.rotate = (scrollPosition / yMax * 180) + 40 + "deg";
+
+    if (scrollPosition >= yMax / 3.98 && scrollPosition <= yMax - yMax / 3.98) {
+        logoS3.classList.remove('hidden')
+        logoS4.classList.remove('hidden')
+    } else {
+        logoS3.classList.add('hidden')
+        logoS4.classList.add('hidden')
+    }
 });
 
